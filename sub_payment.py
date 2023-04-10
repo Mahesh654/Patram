74552d0f7bc11f341d05ffcd6cecbb7ef7c2189c
Mahesh654 <117348247+Mahesh654@users.noreply.github.com>
2023-04-10 15:20:30 +0530

    Add files via upload

---
+++ sub_payment.py
@@ -0,0 +1,50 @@
+from datetime import date 
+from email import message
+from flask import Flask,app,render_template,redirect
+
+import pymysql
+
+
+from flask import jsonify
+
+from flask import flash, request
+import mysql.connector
+
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.secret_key = 'xyzsdfg'
+
+
+
+mydb = mysql.connector.connect(
+
+  host="patram-subscription-db.c4qldlczzvrr.us-east-1.rds.amazonaws.com",
+
+  user="admin",
+
+  password="admin123",
+
+  database="patram"
+)
+
+mysql = MySQL(app)
+
+@app.route('/view', methods =['GET'])
+
+def view_data():
+
+  cursor = mydb.cursor(dictionary=True)
+
+  cursor.execute("SELECT * FROM subsription_payment")
+
+  rows= cursor.fetchall()
+
+    # print (rows)
+
+  return jsonify(rows)
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
